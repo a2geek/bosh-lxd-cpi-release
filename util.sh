3c13d31923f776ffcd85fb2e45eecf816974ab28
Rob Greene <robgreene@gmail.com>
2018-12-23 21:58:05 -0600

    Structure for a test deploy.

--- util.sh
+++ util.sh
@@ -23,6 +23,29 @@ function do_deps() {
   find * -type d -name '.git' | xargs rm -rf
 }
 
+function do_deploy() {
+  bosh_deployment="${BOSH_DEPLOYMENT:-~/Documents/Source/bosh-deployment}"
+  cpi_path=$PWD/cpi
+
+  rm -f creds.yml
+
+  echo "-----> `date`: Create dev release"
+  bosh create-release --force --dir ./../ --tarball $cpi_path
+
+  echo "-----> `date`: Create env"
+  bosh create-env ${bosh_deployment}/bosh.yml \
+    -o ~/workspace/bosh-deployment/docker/cpi.yml \
+    -o ${bosh_deployment}/jumpbox-user.yml \
+    -o ../manifests/dev.yml \
+    --state=state.json \
+    --vars-store=creds.yml \
+    -v docker_cpi_path=$cpi_path \
+    -v director_name=docker \
+    -v internal_cidr=10.245.0.0/16 \
+    -v internal_gw=10.245.0.1 \
+    -v internal_ip=10.245.0.11
+}
+
 if [[ "$0" == "bash" ]]
 then
   alias util="${BASH_SOURCE}"
