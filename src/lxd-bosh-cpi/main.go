4a08422c3c2724de81b8caf514da6cc1e475bbbf
Rob Greene <robgreene@gmail.com>
2018-12-24 16:48:42 -0600

    Adding socket configuration; clean subcommand.

--- src/lxd-bosh-cpi/main.go
+++ src/lxd-bosh-cpi/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+	boshsys "github.com/cloudfoundry/bosh-utils/system"
 	boshuuid "github.com/cloudfoundry/bosh-utils/uuid"
 	"github.com/cppforlife/bosh-cpi-go/apiv1"
 	"github.com/cppforlife/bosh-cpi-go/rpc"
 	lxdclient "github.com/lxc/lxd/client"
 )
 
+var (
+	configPathOpt = flag.String("configPath", "", "Path to configuration file")
+)
+
 func main() {
 	logger := boshlog.NewLogger(boshlog.LevelNone)
+	fs := boshsys.NewOsFileSystem(logger)
 
-	cli := rpc.NewFactory(logger).NewCLI(CPIFactory{})
+	flag.Parse()
 
-	err := cli.ServeOnce()
+	config, err := NewConfigFromPath(*configPathOpt, fs)
+	if err != nil {
+		logger.Error("main", "Loading config %s", err.Error())
+		os.Exit(1)
+	}
+
+	cpiFactory := NewFactory(config)
+
+	cli := rpc.NewFactory(logger).NewCLI(cpiFactory)
+
+	err = cli.ServeOnce()
 	if err != nil {
 		logger.Error("main", "Serving once: %s", err)
 		os.Exit(1)
@@ -23,10 +40,16 @@ func main() {
 }
 
 // CPIFactory implementation.
-type CPIFactory struct{}
+type CPIFactory struct {
+	config Config
+}
+
+func NewFactory(config Config) CPIFactory {
+	return CPIFactory{config}
+}
 
 func (f CPIFactory) New(_ apiv1.CallContext) (apiv1.CPI, error) {
-	c, err := lxdclient.ConnectLXDUnix("", nil)
+	c, err := lxdclient.ConnectLXDUnix(f.config.Server.Socket, nil)
 	if err != nil {
 		return nil, err
 	}
--- util.sh
+++ util.sh
@@ -28,8 +28,28 @@ function do_deps() {
   find * -type d -name '.git' | xargs rm -rf
 }
 
+function do_clean() {
+  echo "Cleaning out all state..."
+  set -x
+  rm -rf .dev_builds/
+  rm -rf dev_releases/
+  rm -f cpi
+  rm -f creds.yml
+  rm -f state.json
+
+  guid_pattern="[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+  lxc list --format json |
+    jq -r '.[] | .name | select(test("c-${guid_pattern}"))' |
+    xargs --verbose --no-run-if-empty --max-args=1 lxc delete -f
+  lxc image list --format json |
+    jq -r '.[] | select(.aliases[0].name | test("img-${guid_pattern}")) | .fingerprint' |
+    xargs --verbose --no-run-if-empty --max-args=1 lxc image delete
+}
+
 function do_deploy() {
+  set -eu
   bosh_deployment="${BOSH_DEPLOYMENT:-${HOME}/Documents/Source/bosh-deployment}"
+  lxd_unix_socket="${LXD_SOCKET:-/var/lib/lxd/unix.socket}"
   cpi_path=$PWD/cpi
 
   rm -f creds.yml
@@ -45,6 +65,7 @@ function do_deploy() {
     --vars-store=creds.yml \
     -v cpi_path=$cpi_path \
     -v director_name=lxd \
+    -v lxd_unix_socket=$lxd_unix_socket \
     -v internal_cidr=10.245.0.0/16 \
     -v internal_gw=10.245.0.1 \
     -v internal_ip=10.245.0.11
