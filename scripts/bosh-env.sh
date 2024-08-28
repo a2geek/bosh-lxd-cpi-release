7f5d289208cf8465f761fbb3587b4458b94311a8
Rob Greene <robgreene@gmail.com>
2024-08-28 12:32:42 -0500

    Adding SSH cleanup into script since it needs to be run a lot.

--- scripts/bosh-env.sh
+++ scripts/bosh-env.sh
@@ -5,4 +5,5 @@ pushd $(dirname ${BASH_SOURCE})/.. > /dev/null
   export BOSH_CLIENT_SECRET=$(bosh int creds/bosh.yml --path /admin_password)
   export BOSH_ENVIRONMENT=10.245.0.11
   export BOSH_CA_CERT=$(bosh int creds/bosh.yml --path /director_ssl/ca)
+  ssh-keygen -f ~/.ssh/known_hosts -R 10.245.0.11
 popd > /dev/null
