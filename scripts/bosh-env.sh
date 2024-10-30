af7ef8882e4df60c641cb30cf458f72b02c785ab
Rob Greene <robgreene@gmail.com>
2024-10-30 11:00:11 -0500

    Tweak to pull BOSH IP from the config file dynamically.

--- scripts/bosh-env.sh
+++ scripts/bosh-env.sh
@@ -3,6 +3,6 @@
 pushd $(dirname ${BASH_SOURCE})/.. > /dev/null
   export BOSH_CLIENT=admin
   export BOSH_CLIENT_SECRET=$(bosh int creds/bosh.yml --path /admin_password)
-  export BOSH_ENVIRONMENT=10.245.0.11
+  export BOSH_ENVIRONMENT=$(bosh int manifests/bosh-vars.yml --path /internal_ip)
   export BOSH_CA_CERT=$(bosh int creds/bosh.yml --path /director_ssl/ca)
 popd > /dev/null
