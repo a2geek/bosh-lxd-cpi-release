790cb921ee63a8dbc5f9a458f64c67c9ff88b8ac
Rob Greene <robgreene@gmail.com>
2026-03-11 16:25:13 -0500

    The stop / start action caused the VM to remain in a stopped state when detaching disks. Remove dthat. BATs pass now with and without the agent.

--- src/cpi/detach_disk.go
+++ src/cpi/detach_disk.go
@@ -1,19 +1,12 @@
 package cpi
 
 import (
-	"bosh-lxd-cpi/adapter"
-
 	"github.com/cloudfoundry/bosh-cpi-go/apiv1"
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 )
 
 func (c CPI) DetachDisk(vmCID apiv1.VMCID, diskCID apiv1.DiskCID) error {
-	err := c.adapter.SetInstanceAction(vmCID.AsString(), adapter.StopAction)
-	if err != nil {
-		return bosherr.WrapError(err, "Stopping instance")
-	}
-
-	err = c.adapter.DetachDeviceBySource(vmCID.AsString(), diskCID.AsString())
+	err := c.adapter.DetachDeviceBySource(vmCID.AsString(), diskCID.AsString())
 	if err != nil {
 		return bosherr.WrapError(err, "Detach Device")
 	}
@@ -28,11 +21,6 @@ func (c CPI) DetachDisk(vmCID apiv1.VMCID, diskCID apiv1.DiskCID) error {
 	err = c.writeAgentFileToVM(vmCID, agentEnv)
 	if err != nil {
 		return bosherr.WrapError(err, "Write AgentEnv")
-	}
-
-	err = c.adapter.SetInstanceAction(vmCID.AsString(), adapter.StartAction)
-	if err != nil {
-		return bosherr.WrapError(err, "Starting instance")
 	}
 
 	return nil
