d0d8885596f1a599b3c179e2f769d189edd2f762
a2geek <robgreene@gmail.com>
2026-07-09 11:38:00 -0500

    Making predeploy VM stick around and placing an english description on said VM.

--- src/cpi/delete_stemcell.go
+++ src/cpi/delete_stemcell.go
@@ -11,5 +11,19 @@ func (c CPI) DeleteStemcell(cid apiv1.StemcellCID) error {
 	}
 
 	alias := cid.AsString()
+
+	// Look for the predeploy VM and delete it if it exists.
+	predeployVMName := c.makeStemcellPredeployVMName(alias)
+	exists, err := c.HasVM(apiv1.NewVMCID(predeployVMName))
+	if err != nil {
+		return nil
+	}
+	if exists {
+		err = c.adapter.DeleteInstance(predeployVMName)
+		if err != nil {
+			return err
+		}
+	}
+
 	return c.adapter.DeleteImage(alias)
 }
